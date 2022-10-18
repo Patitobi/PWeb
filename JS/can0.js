fa305c03cd13abecbe8cf6ebb583183ba3705cdf
Patitobi <100159085+Patitobi@users.noreply.github.com>
2022-10-18 23:04:11 +0200

    Add files via upload

---
+++ JS/can0.js
@@ -0,0 +1,26 @@
+var canvas = document.querySelector('canvas');
+var c = canvas.getContext('2d');
+var inwi;
+var inhe;
+
+canvas.height=400;
+canvas.widt=300;
+
+c.beginPath();
+c.moveTo(20,400);
+c.bezierCurveTo(20, 200, 280, 200, 280, 400);
+c.fillStyle='#fff';
+c.fill();
+
+c.beginPath();
+c.arc(150, 150, 50, 0, Math.PI*2, false);
+c.fillStyle='#fff';
+c.fill();
+
+function animate(){
+    requestAnimationFrame(animate);
+    inwi=window.innerHeight;
+    inhe=window.innerHeight;
+
+}
+animate();
