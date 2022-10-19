f173bda96e65084e6eccc71489933f41a75f1cc7
Patitobi <100159085+Patitobi@users.noreply.github.com>
2022-10-19 09:28:54 +0200

    Update can0.js

--- JS/can0.js
+++ JS/can0.js
@@ -1,7 +1,5 @@
 var canvas = document.querySelector('canvas');
 var c = canvas.getContext('2d');
-var inwi;
-var inhe;
 
 canvas.height=400;
 canvas.widt=300;
@@ -16,11 +14,3 @@ c.beginPath();
 c.arc(150, 150, 50, 0, Math.PI*2, false);
 c.fillStyle='#fff';
 c.fill();
-
-function animate(){
-    requestAnimationFrame(animate);
-    inwi=window.innerHeight;
-    inhe=window.innerHeight;
-
-}
-animate();
